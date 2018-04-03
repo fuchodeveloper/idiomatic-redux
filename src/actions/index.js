cd14bc3e156a9302ee4c80549fe735baf1716f59
fuchodeveloper <fredrick.mgbeoma@andela.com>
2018-04-03 09:15:41 +0100

    navigating with React Router Link

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,15 +5,10 @@ let nextTodoId = 0;
 export const addTodo = text => ({
   type: 'ADD_TODO',
   id: v4(),
-  text
-});
-
-export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
+  text,
 });
 
 export const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
-  id
-})
+  id,
+});
