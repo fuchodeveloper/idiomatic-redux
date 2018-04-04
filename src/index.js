4c374d4a75c8071862c4bcefd7a5cf5894195d1c
fuchodeveloper <fredrick.mgbeoma@andela.com>
2018-04-04 14:04:03 +0100

    setup in-memory rest database

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 import configureStore from './configureStore';
+import { fetchTodos } from './api';
+
+fetchTodos('all').then(todos => {
+  console.group('TODOS');
+  console.log('%c', 'color: blue', todos);
+  console.groupEnd('TODOS');
+}
+);
 
 const store = configureStore();
 
